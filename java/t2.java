3c0868d05ecd2a53b43edb642f6b593ee4c46755
MiniCash <tminecraft2143@gmail.com>
2026-01-07 21:47:32 +0900

    Create t2.java

---
+++ java/t2.java
@@ -0,0 +1,21 @@
+public class t2 {
+
+    public static void main(String[] args) {
+        if (args.length == 2) {
+            try{
+                int arg1 = Integer.parseInt(args[0]);
+                int arg2 = Integer.parseInt(args[1]);
+
+                String message = Integer.toString(arg1 + arg2);
+                
+                System.out.println(message);
+
+            }catch(Exception e){
+                System.out.println("入力例：<1>_<2>");
+            }
+        }else{
+            System.out.println("引数が足りません");
+        }
+    }
+    
+}
